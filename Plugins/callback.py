3088aaaed3edb672d2fe91599a5df07843ed09a2
TG-PushpaReju <97326699+TG-chembanreju@users.noreply.github.com>
2022-04-12 11:20:33 +0530

    Update callback.py

--- Plugins/callback.py
+++ Plugins/callback.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 @Client.on_callback_query()
 async def cb(bot, msg):
-    if msg.data == "about"
+    if msg.data == "about":
         await msg.answer("""🕹️ Bot Type :  Pyrogram Tester
 ⚙️ Language : Python 3.9.9
 📚 Library : Pyrogram Asyncio 1.13.0 
